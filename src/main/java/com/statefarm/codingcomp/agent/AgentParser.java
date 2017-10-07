b0416dc6dc596589ac2eaaae07dcdb869b4ab609
Ishan <ishans@usc.edu>
2017-10-07 11:14:37 -0700

    Got the file read in

--- src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
+++ src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
@@ -1,5 +1,10 @@
 package com.statefarm.codingcomp.agent;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -14,6 +19,16 @@ public class AgentParser {
 
 	@Cacheable(value = "agents")
 	public Agent parseAgent(String fileName) {
+		Agent agent = new Agent(); 
+		File input = new File(fileName); 
+		
+		try {
+			Document doc = Jsoup.parse(input, "UTF-8", "");
+		} catch (IOException e) {
+			System.out.println("Reading HTML failed!"); 
+			e.printStackTrace();
+		} 
+		
 		return null;
 	}
 }
